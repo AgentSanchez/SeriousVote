07d2578ea90ba575e8fe5d6ceb651246d725868f
BuildTools <unconfigured@null.spigotmc.org>
2016-12-14 02:51:27 -0800

    Setup Config Loader

--- src/main/java/net/adamsanchez/seriousvote/SeriousVote.java
+++ src/main/java/net/adamsanchez/seriousvote/SeriousVote.java
@@ -5,7 +5,8 @@ import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.sponge.event.VotifierEvent;
 
 
-
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.ConfigurationOptions;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.Server;
@@ -18,6 +19,7 @@ import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.cause.Cause;
@@ -26,11 +28,23 @@ import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.game.state.GamePostInitializationEvent;
 import org.spongepowered.api.plugin.Dependency;
 import org.spongepowered.api.plugin.Plugin;
+
 import org.spongepowered.api.service.economy.EconomyService;
 import org.spongepowered.api.service.economy.account.UniqueAccount;
 import org.spongepowered.api.text.Text;
+
 import org.spongepowered.api.text.channel.MessageChannel;
 import org.spongepowered.api.text.format.TextColors;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by adam_ on 12/08/16.
@@ -57,6 +71,19 @@ public class SeriousVote
     }
 
 
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    private Path defaultConfig;
+
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    private ConfigurationLoader<CommentedConfigurationNode> configManager;
+
+    @Inject
+    @ConfigDir(sharedRoot = false)
+    private Path privateConfigDir;
+
+
     @Listener
     public void onServerStart(GameInitializationEvent event)
     {
@@ -64,7 +91,53 @@ public class SeriousVote
         seriousVotePlugin = this;
         game = Sponge.getGame();
 
-        //TODO:Load Configurations
+        //Load Configurations
+        ConfigurationLoader<CommentedConfigurationNode> loader =
+                HoconConfigurationLoader.builder().setPath(defaultConfig).build();
+        ConfigurationNode rootNode = loader.createEmptyNode(ConfigurationOptions.defaults());
+
+        try {
+            rootNode = loader.load();
+        } catch(IOException e) {
+            getLogger().warn("THERE WAS AN ERROR WITH THE SERIOUS VOTE CONFIGURATION FILE");
+        }
+
+        ConfigurationNode defaultNode;
+        defaultNode = loader.createEmptyNode();
+        if (rootNode.getChildrenList().size() == 0){
+            getLogger().info("No configuration found... Attempting to load Default Configurations...");
+            URL jarConfigFile = this.getClass().getResource("defaultConfig.conf");
+            ConfigurationLoader<CommentedConfigurationNode> defaultLoader =
+                    HoconConfigurationLoader.builder().setURL(jarConfigFile).build();
+
+
+            try {
+                defaultNode = defaultLoader.load();
+
+                if(defaultNode.getChildrenList().size() == 0) {
+                    try {
+                        loader.save(defaultNode);
+                        getLogger().info("Default Configurations Loaded");
+                        rootNode = defaultNode;
+                    } catch (IOException e) {
+                        getLogger().warn("THERE WAS AN ERROR TRYING TO SAVE THE DEFAULT CONFIGURATIONS");
+                    }
+                }
+                else
+                {
+                    getLogger().warn("No configurations could be loaded for Serious Vote, we will now shut down.");
+                }
+
+            } catch(IOException e) {
+                getLogger().warn("THERE WAS AN ERROR WITH THE SERIOUS VOTE DEFAULT CONFIGURATION FILE");
+            }
+
+
+        }
+
+
+
+
         CommandSpec vote = CommandSpec.builder()
                 .description(Text.of("Checks to see if it's running"))
                 .permission("nope.nope.nope")
@@ -103,7 +176,15 @@ public class SeriousVote
     }
 
     public boolean rewardVote(String username){
-        game.getCommandManager().process(game.getServer().getConsole(), "say Hello!! " + username + " Just voted");
+        List<String> commandList = new ArrayList<String>();
+
+
+        for (String command : commandList) {
+            game.getCommandManager().process(game.getServer().getConsole(), command);
+        }
+
+
+
         return true;
     }
 
