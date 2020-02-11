089dd64a619a46c495d345aeb573a2f574a116aa
Adam Sanchez <adam_sanchez@corruptiongames.net>
2020-02-10 21:55:32 -0800

    DB changes

--- src/main/java/net/adamsanchez/seriousvote/Data/Database.java
+++ src/main/java/net/adamsanchez/seriousvote/Data/Database.java
@@ -32,9 +32,7 @@ public class Database {
     HikariDataSource ds;
 
 
-
-
-    public Database(){
+    public Database() {
 
         SeriousVote sv = SeriousVote.getInstance();
         this.host = sv.databaseHostname;
@@ -46,14 +44,14 @@ public class Database {
         try {
             this.minIdleConnections = Integer.parseInt(sv.minIdleConnections);
             this.maxActiveConnections = Integer.parseInt(sv.maxActiveConnections);
-        } catch(Exception e) {
+        } catch (Exception e) {
             U.info("Incorrect values given for connection pool, reverting to default");
             U.info("Max Active: " + maxActiveConnections);
             U.info("Min Idle: " + minIdleConnections);
         }
         playerTable = table_prefix + "players";
 
-        url = "jdbc:mysql://"+ host + ":" + port + "/" + dbname + "?useSSL=false";
+        url = "jdbc:mysql://" + host + ":" + port + "/" + dbname + "?useSSL=false";
 
         config.setJdbcUrl(url + timezoneFix);
         config.setUsername(username);
@@ -69,7 +67,7 @@ public class Database {
         U.info("Ready for connections");
     }
 
-    public Database(String url, String username, String password){
+    public Database(String url, String username, String password) {
         config.setJdbcUrl(url + timezoneFix);
         config.setUsername(username);
         config.setPassword(password);
@@ -84,16 +82,12 @@ public class Database {
     }
 
 
-
-
-
-
     /////////////////////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    public void shutdown(){
+    public void shutdown() {
         ds.close();
     }
 
@@ -102,18 +96,17 @@ public class Database {
     }
 
 
-    public PreparedStatement preparedStatement(Connection con, String string){
+    public PreparedStatement preparedStatement(Connection con, String string) {
         PreparedStatement statement = null;
-        try{
+        try {
             statement = con.prepareStatement(string);
-
         } catch (SQLException e) {
             U.error("Error in DB Connection");
         }
         return statement;
     }
 
-    private ResultSet genericQuery(Connection con, String query){
+    private ResultSet genericQuery(Connection con, String query) {
         ResultSet results = null;
         try {
             results = con.createStatement().executeQuery(query);
@@ -124,27 +117,29 @@ public class Database {
     }
 
 
-    public ResultSet genericSelectQuery(Connection con, String table, String field, String value){
+    public ResultSet genericSelectQuery(Connection con, String table, String field, String value) {
         String initial = "SELECT * FROM %s WHERE %s='%s'";
-        ResultSet results = genericQuery(con, String.format(initial,table,field,value));
-        return  results;
+        ResultSet results = genericQuery(con, String.format(initial, table, field, value));
+        return results;
     }
 
     /**
      * Gets an ordered list of records starting at the nth record (offset) using no Limit
+     *
      * @param con
      * @param table
      * @param orderByField
      * @return
      */
-    public ResultSet orderedSelectQuery(Connection con, String table, String orderByField){
+    public ResultSet orderedSelectQuery(Connection con, String table, String orderByField) {
         String initial = "SELECT * FROM %s ORDER BY %s DESC";
-        ResultSet results = genericQuery(con, String.format(initial,table,orderByField));
-        return  results;
+        ResultSet results = genericQuery(con, String.format(initial, table, orderByField));
+        return results;
     }
 
     /**
      * Gets an ordered list of records starting at the nth record (offset) with a limit
+     *
      * @param con
      * @param table
      * @param orderByField
@@ -152,125 +147,182 @@ public class Database {
      * @param offset
      * @return
      */
-    public ResultSet orderedSelectQuery(Connection con, String table, String orderByField, int limit, int offset){
+    public ResultSet orderedSelectQuery(Connection con, String table, String orderByField, int limit, int offset) {
         String initial = "SELECT * FROM %s ORDER BY %s DESC LIMIT %s OFFSET %s";
-        ResultSet results = genericQuery(con, String.format(initial,table,orderByField,limit,offset));
-        return  results;
+        ResultSet results = genericQuery(con, String.format(initial, table, orderByField, limit, offset));
+        return results;
     }
 
 
-
     //////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////////////////
 
-    public PlayerRecord getPlayer(String playerIdentifier){
+    public PlayerRecord getPlayer(String playerIdentifier) {
         ResultSet results = null;
-        try(Connection con = getConnection()){
-             results = genericSelectQuery(con, playerTable, "player", playerIdentifier.toString());
-            if(results.first()){
+        Connection con = null;
+        try {
+            con = getConnection();
+            results = genericSelectQuery(con, playerTable, "player", playerIdentifier.toString());
+            if (results.first()) {
                 int sequentialVotes = results.getInt("voteSpree");
                 Date lastVote = results.getDate("lastVote");
                 int totalVote = results.getInt("totalVotes");
-                return new PlayerRecord(playerIdentifier, totalVote,sequentialVotes,lastVote);
+                return new PlayerRecord(playerIdentifier, totalVote, sequentialVotes, lastVote);
             }
         } catch (SQLException e) {
             U.error("Trouble getting information from the database");
+        } finally {
+            try {
+                if (results != null) {
+                    results.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+            }
         }
         return null;
     }
 
     /**
      * Retrieves the nth row from the cache if it exists
+     *
      * @param rank The n'th record
      * @return
      */
-    public PlayerRecord getRecordByRank(int rank){
+    public PlayerRecord getRecordByRank(int rank) {
         ArrayList<PlayerRecord> cache = getAllRecords();
-        if(rank < cache.size()) {
+        if (rank < cache.size()) {
             return cache.get(rank);
         } else {
-            U.debug("There are not that many records!!! Requsted " + (rank+1) );
+            U.debug("There are not that many records!!! Requsted " + (rank + 1));
             return null;
         }
     }
 
-    public void updatePlayer(PlayerRecord player){
+    public void updatePlayer(PlayerRecord player) {
         playerUpdateQuery(this.playerTable, player.getPlayerIdentifier(), player.getTotalVotes(), player.getVoteSpree(), player.getLastVote());
     }
 
     /**
      * Sets all player votes to 0
      */
-    public void resetPlayers(){
+    public void resetPlayers() {
         String query = String.format("UPDATE %s SET totalVotes = 0", playerTable);
-
-        try(Connection con = getConnection()){
-            PreparedStatement statement = preparedStatement(con, query);
+        Connection con = null;
+        PreparedStatement statement = null;
+        try {
+            con = getConnection();
+            statement = preparedStatement(con, query);
             statement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+            }
         }
     }
 
-    public void deletePlayer(String playerIdentifier){
+    public void deletePlayer(String playerIdentifier) {
         String query = String.format("DELETE FROM %s WHERE player='%s';", playerTable, playerIdentifier);
-         try(Connection con = getConnection()){
-             PreparedStatement statement = preparedStatement(con,query);
-             statement.execute();
-         } catch (SQLException e) {
-             e.printStackTrace();
-         }
+        PreparedStatement statement = null;
+        Connection con = null;
+
+        try {
+            con = getConnection();
+            statement = preparedStatement(con, query);
+            statement.execute();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+
     }
 
-    public ArrayList<PlayerRecord> getAllRecords(){
+    public ArrayList<PlayerRecord> getAllRecords() {
         //First Check Cache
         //Has it been initialized
         U.debug("Cache Expires at: " + recordCacheExpirationTime + " Current Time: " + new java.util.Date().getTime());
 
-        if(recordCache == null || recordCache.size() == 0){
+        if (recordCache == null || recordCache.size() == 0) {
             U.debug("Cache initializing....");
             recordCacheExpirationTime = new java.util.Date().getTime() + cacheLifetime;
             return updateAllPlayerCache();
         } else {
             U.debug("Checking cache expiration");
             //If time has expired
-            if(recordCacheExpirationTime < new java.util.Date().getTime()){
+            if (recordCacheExpirationTime < new java.util.Date().getTime()) {
                 recordCacheExpirationTime = new java.util.Date().getTime() + cacheLifetime;
                 U.debug("Refreshing cache");
                 return updateAllPlayerCache();
-            } else{
+            } else {
                 U.debug("Cache still fresh...");
                 return recordCache;
             }
         }
     }
 
-    private ArrayList<PlayerRecord> updateAllPlayerCache(){
+    private ArrayList<PlayerRecord> updateAllPlayerCache() {
         ArrayList<PlayerRecord> recordList = new ArrayList<>();
-        ResultSet results;
-        try(Connection con = getConnection()) {
-            results = orderedSelectQuery(con,playerTable, "totalVotes");
+        ResultSet results = null;
+        Connection con = null;
+
+
+        try {
+            con = getConnection();
+            results = orderedSelectQuery(con, playerTable, "totalVotes");
             U.debug("Received " + results.getFetchSize());
-            while(results.next()){
+            while (results.next()) {
                 int sequentialVotes = results.getInt("voteSpree");
                 Date lastVote = results.getDate("lastVote");
                 int totalVote = results.getInt("totalVotes");
                 String playerIdentifier = results.getString("player");
-                recordList.add(new PlayerRecord(playerIdentifier, totalVote,sequentialVotes,lastVote));
+                recordList.add(new PlayerRecord(playerIdentifier, totalVote, sequentialVotes, lastVote));
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (results != null) {
+                    results.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+            }
         }
         recordCache = recordList;
         return recordCache;
     }
 
-    public void playerUpdateQuery(String table, String playerIdentifier, int totalVotes, int voteSpree, Date lastVote){
+    public void playerUpdateQuery(String table, String playerIdentifier, int totalVotes, int voteSpree, Date lastVote) {
         String initial = "REPLACE INTO %s(player, totalVotes, voteSpree, lastVote) VALUES(?,?,?,?)";
+        PreparedStatement statement = null;
+        Connection con = null;
 
-        try(Connection con = getConnection()){
-            PreparedStatement statement = preparedStatement(con, String.format(initial,table));
+        try {
+            con = getConnection();
+            statement = preparedStatement(con, String.format(initial, table));
             statement.setString(1, playerIdentifier);
             statement.setInt(2, totalVotes);
             statement.setInt(3, voteSpree);
@@ -278,21 +330,44 @@ public class Database {
             statement.execute();
         } catch (SQLException e) {
             U.error("Error in trying to update player vote record!");
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+            }
         }
     }
 
-    public void createPlayerTable(){
+    public void createPlayerTable() {
         String table = String.format("CREATE TABLE IF NOT EXISTS %s(" +
                 "player			VarChar(36) PRIMARY KEY," +
                 "lastVote		DATE," +
                 "totalVotes		INT," +
                 "voteSpree		INT" +
                 ")", playerTable);
-
-        try(Connection con = getConnection()){
-            con.createStatement().executeUpdate(table);
+        Connection con = null;
+        Statement statement = null;
+        try {
+            con = getConnection();
+            statement = con.createStatement();
+            statement.executeUpdate(table);
         } catch (SQLException e) {
             U.error("Error Creating SQL TABLE-- CHECK YOUR DATA CONFIG", e);
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+            }
         }
 
     }
@@ -300,23 +375,38 @@ public class Database {
     /**
      * Returns the number of players in the table
      */
-    public int getCount(){
+    public int getCount() {
         ResultSet results = null;
+        Connection con = null;
+        Statement statement = null;
+
         int count = 0;
-        String query = String.format("SELECT COUNT(*) FROM %s;",playerTable);
-        try(Connection con = getConnection()){
+        String query = String.format("SELECT COUNT(*) FROM %s;", playerTable);
+        try {
+            con = getConnection();
             results = con.createStatement().executeQuery(query);
-            while(results.next()){
+            while (results.next()) {
                 count = results.getInt(1);
                 U.debug("Table has " + count + " players.");
                 return count;
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (results != null) {
+                    results.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+            }
         }
         return count;
     }
-
-
 
 }
