0e5ff2f69b8d050001436264f5e1ca59bed4e0dc
Adam Sanchez <adam_sanchez@corruptiongames.net>
2020-05-29 04:27:20 -0700

    Fix le return

--- src/main/java/net/adamsanchez/seriousvote/commands/VoteCommand.java
+++ src/main/java/net/adamsanchez/seriousvote/commands/VoteCommand.java
@@ -56,6 +56,7 @@ public class VoteCommand implements CommandExecutor {
                         }
                     }
                 }
+
             }
         }
         return CommandResult.success();
