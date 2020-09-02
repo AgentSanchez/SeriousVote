062cde57344e2fe1d156259e75797c71e5942151
Adam Sanchez <adam_sanchez@corruptiongames.net>
2020-09-02 01:37:15 -0700

    Update VoteRequest.java

--- src/main/java/net/adamsanchez/seriousvote/vote/VoteRequest.java
+++ src/main/java/net/adamsanchez/seriousvote/vote/VoteRequest.java
@@ -2,12 +2,15 @@ package net.adamsanchez.seriousvote.vote;
 
 import com.vexsoftware.votifier.model.Vote;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class VoteRequest extends Vote {
     private List<String> rewardStore = new LinkedList<String>();
     private Status voteStatus = Status.WAITING;
+    private Set<String> rewardNames = new HashSet<String>();
 
     public void addReward(String command){
         rewardStore.add(command);
@@ -15,6 +18,17 @@ public class VoteRequest extends Vote {
 
     public List<String> getRewards(){
         return rewardStore;
+    }
+
+    public boolean hasRewards(){
+        return !rewardStore.isEmpty();
+    }
+
+    public Status getVoteStatus(){
+        return voteStatus;
+    }
+    public void setVoteStatus(Status status){
+        this.voteStatus = status;
     }
 
 
