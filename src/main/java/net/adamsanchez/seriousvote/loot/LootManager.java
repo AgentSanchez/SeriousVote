73e6035f6d0b6711d6811444c33a6a16a9b29e1a
Adam Sanchez <adam_sanchez@corruptiongames.net>
2020-09-07 04:37:05 -0700

    Update access

--- src/main/java/net/adamsanchez/seriousvote/loot/LootManager.java
+++ src/main/java/net/adamsanchez/seriousvote/loot/LootManager.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class LootManager {
 
-    String[][] mainRewardTables;
+    private String[][] mainRewardTables;
     private int[] chanceMap;
     private static LootManager instance;
 
