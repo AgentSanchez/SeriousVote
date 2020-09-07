51d468ccc4912eb2b7fa87c7118d045fcfdfa3a5
Adam Sanchez <adam_sanchez@corruptiongames.net>
2020-09-07 02:23:12 -0700

    Begin migrating config and vars away from main class.

--- src/main/java/net/adamsanchez/seriousvote/utils/CM.java
+++ src/main/java/net/adamsanchez/seriousvote/utils/CM.java
@@ -1,20 +1,15 @@
 package net.adamsanchez.seriousvote.utils;
 
-import com.google.common.collect.Iterables;
-import jdk.nashorn.internal.runtime.regexp.joni.Config;
 import net.adamsanchez.seriousvote.SeriousVote;
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.spongepowered.api.asset.Asset;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 /**
@@ -22,124 +17,137 @@ import java.util.stream.Collectors;
  */
 public class CM {
 
+    private static CM instance;
+    private static ConfigurationNode mainCfgNode;
+    ConfigurationLoader<CommentedConfigurationNode> loader;
 
-    public static ConfigurationNode RootNode;
-
-    public static boolean getDebugMode(ConfigurationNode node){
-        return node.getNode("config","debug-mode").getBoolean();
+    public static boolean getDebugMode() {
+        return get().mainCfgNode.getNode("config", "debug-mode").getBoolean();
     }
 
-    public static List<String> getWeeklySetCommands(ConfigurationNode node){
-        return node.getNode("config","dailies","weekly","set").getChildrenList().stream()
-                .map(ConfigurationNode::getString).collect(Collectors.toList());
-    }
-    public static List<String> getMonthlySetCommands(ConfigurationNode node){
-        return node.getNode("config","dailies","monthly","set").getChildrenList().stream()
-                .map(ConfigurationNode::getString).collect(Collectors.toList());
-    }
-    public static List<String> getYearlySetCommands(ConfigurationNode node){
-        return node.getNode("config","dailies","yearly","set").getChildrenList().stream()
+    public static List<String> getWeeklySetCommands() {
+        return get().mainCfgNode.getNode("config", "dailies", "weekly", "set").getChildrenList().stream()
                 .map(ConfigurationNode::getString).collect(Collectors.toList());
     }
 
-    public static int[] getEnabledMilestones(ConfigurationNode node){
-        List<String> list = node.getNode("config","milestones","records-enabled").getChildrenList().stream()
+    public static List<String> getMonthlySetCommands() {
+        return get().mainCfgNode.getNode("config", "dailies", "monthly", "set").getChildrenList().stream()
+                .map(ConfigurationNode::getString).collect(Collectors.toList());
+    }
+
+    public static List<String> getYearlySetCommands() {
+        return get().mainCfgNode.getNode("config", "dailies", "yearly", "set").getChildrenList().stream()
+                .map(ConfigurationNode::getString).collect(Collectors.toList());
+    }
+
+    public static int[] getEnabledMilestones() {
+        List<String> list = get().mainCfgNode.getNode("config", "milestones", "records-enabled").getChildrenList().stream()
                 .map(ConfigurationNode::getString).collect(Collectors.toList());
         int[] array = new int[list.size()];
-        for(int ix = 0; ix < list.size(); ix++){
-            array[ix]=Integer.parseInt(list.get(ix));
+        for (int ix = 0; ix < list.size(); ix++) {
+            array[ix] = Integer.parseInt(list.get(ix));
         }
 
         return array;
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////
-    public static String getDatabaseType(ConfigurationNode node){
-        return node.getNode("config","database","dbType").getString();
-    }
-    public static String getDatabaseName(ConfigurationNode node){
-        return node.getNode("config","database","name").getString();
-    }
-    public static String getDatabaseHostname(ConfigurationNode node){
-        return node.getNode("config","database","hostname").getString();
-    }
-    public static String getDatabasePort(ConfigurationNode node){
-        return node.getNode("config","database","port").getString();
-    }
-    public static String getDatabasePrefix(ConfigurationNode node){
-        return node.getNode("config","database","prefix").getString();
-    }
-    public static String getDatabaseUsername(ConfigurationNode node){
-        return node.getNode("config","database","username").getString();
-    }
-    public static String getDatabasePassword(ConfigurationNode node){
-        return node.getNode("config","database","password").getString();
+    public static String getDatabaseType() {
+        return get().mainCfgNode.getNode("config", "database", "dbType").getString();
     }
 
-    public static String getMaxActiveConnections(ConfigurationNode node){
-        return node.getNode("config","database","maximum-active-connections").getString();
-    }
-    public static String getMinIdleConnections(ConfigurationNode node){
-        return node.getNode("config","database","minimum-idle-connections").getString();
+    public static String getDatabaseName() {
+        return get().mainCfgNode.getNode("config", "database", "name").getString();
     }
 
-    public static boolean getMilestonesEnabled(CommentedConfigurationNode node) {
-        return node.getNode("config","milestones","enabled").getBoolean();
+    public static String getDatabaseHostname() {
+        return get().mainCfgNode.getNode("config", "database", "hostname").getString();
     }
 
-    public static boolean getDailiesEnabled(CommentedConfigurationNode node) {
-        return node.getNode("config","dailies","enabled").getBoolean();
+    public static String getDatabasePort() {
+        return get().mainCfgNode.getNode("config", "database", "port").getString();
     }
 
-    public static boolean getMonthlyResetEnabled(CommentedConfigurationNode node) {
-        return node.getNode("config","monthly-reset-enabled").getBoolean();
+    public static String getDatabasePrefix() {
+        return get().mainCfgNode.getNode("config", "database", "prefix").getString();
     }
 
-    public static int getMonthlyResetDay(CommentedConfigurationNode node) {
-        return node.getNode("config", "monthly-reset-day").getInt();
+    public static String getDatabaseUsername() {
+        return get().mainCfgNode.getNode("config", "database", "username").getString();
     }
 
-    public static boolean getMonthlyResetWithOffline(CommentedConfigurationNode node){
-        return node.getNode("config", "monthly-reset-cache-enabled").getBoolean(true);
+    public static String getDatabasePassword() {
+        return get().mainCfgNode.getNode("config", "database", "password").getString();
+    }
+
+    public static String getMaxActiveConnections() {
+        return get().mainCfgNode.getNode("config", "database", "maximum-active-connections").getString();
+    }
+
+    public static String getMinIdleConnections() {
+        return get().mainCfgNode.getNode("config", "database", "minimum-idle-connections").getString();
+    }
+
+    public static boolean getMilestonesEnabled() {
+        return get().mainCfgNode.getNode("config", "milestones", "enabled").getBoolean();
+    }
+
+    public static boolean getDailiesEnabled() {
+        return get().mainCfgNode.getNode("config", "dailies", "enabled").getBoolean();
+    }
+
+    public static boolean getMonthlyResetEnabled() {
+        return get().mainCfgNode.getNode("config", "monthly-reset-enabled").getBoolean();
+    }
+
+    public static int getMonthlyResetDay() {
+        return get().mainCfgNode.getNode("config", "monthly-reset-day").getInt();
+    }
+
+    public static boolean getMonthlyResetWithOffline() {
+        return get().mainCfgNode.getNode("config", "monthly-reset-cache-enabled").getBoolean(true);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////
 
-    public static List<String> getSetCommands(ConfigurationNode node) {
-        return node.getNode("config","vote-reward","set").getChildrenList().stream()
+    public static List<String> getSetCommands() {
+        return get().mainCfgNode.getNode("config", "vote-reward", "set").getChildrenList().stream()
                 .map(ConfigurationNode::getString).collect(Collectors.toList());
     }
-    public static List<String> getRandomCommands(ConfigurationNode node) {
-        return node.getNode("config","vote-reward","random").getChildrenList().stream()
+
+    public static List<String> getRandomCommands() {
+        return get().mainCfgNode.getNode("config", "vote-reward", "random").getChildrenList().stream()
                 .map(ConfigurationNode::getString).collect(Collectors.toList());
     }
-    public static List<String> getVoteSites(ConfigurationNode node) {
-        return node.getNode("config","vote-sites").getChildrenList().stream()
+
+    public static List<String> getVoteSites() {
+        return get().mainCfgNode.getNode("config", "vote-sites").getChildrenList().stream()
                 .map(ConfigurationNode::getString).collect(Collectors.toList());
     }
 
     //Returns the string value from the Config for the public message. This must be deserialized
-    public static String getPublicMessage(ConfigurationNode node){
-        return node.getNode("config","broadcast-message").getString();
+    public static String getPublicMessage() {
+        return get().mainCfgNode.getNode("config", "broadcast-message").getString();
     }
 
-    public static String getOfflineMessage(ConfigurationNode node){
-        return node.getNode("config","broadcast-message-offline").getString();
+    public static String getOfflineMessage() {
+        return get().mainCfgNode.getNode("config", "broadcast-message-offline").getString();
     }
 
-    public static String getVoteSiteMessage(ConfigurationNode node){
-        return node.getNode("config","vote-sites-message").getString();
+    public static String getVoteSiteMessage() {
+        return get().mainCfgNode.getNode("config", "vote-sites-message").getString();
     }
 
-    public static boolean getBypassOffline(CommentedConfigurationNode node) {
-        return node.getNode("config","bypass-offline").getBoolean();
+    public static boolean getBypassOffline() {
+        return get().mainCfgNode.getNode("config", "bypass-offline").getBoolean();
     }
-    public static boolean getMessageOffline(CommentedConfigurationNode node) {
-        return node.getNode("config","broadcast-offline").getBoolean();
+
+    public static boolean getMessageOffline() {
+        return get().mainCfgNode.getNode("config", "broadcast-offline").getBoolean();
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////
-    public static void initConfig(Path pluginDirectory){
+    public static void initConfig(Path pluginDirectory) {
         Asset configAsset = SeriousVote.getInstance().getPlugin().getAsset("seriousvote.conf").orElse(null);
         if (Files.notExists(pluginDirectory)) {
             if (configAsset != null) {
@@ -158,6 +166,30 @@ public class CM {
                 return;
             }
         }
+    }
+
+
+    public CM(ConfigurationLoader<CommentedConfigurationNode> loader) {
+        instance = this;
+        this.loader = loader;
+    }
+
+    public boolean loadConfig() {
+        try {
+            mainCfgNode = loader.load();
+            U.info(CC.GREEN + "SV Configuration has been loaded.");
+        } catch (IOException e) {
+            U.error(CC.RED + "There was an error while reloading your configs");
+            U.error(CC.RED_UNDERLINED + "PLEASE CHECK YOUR CONFIG FOR MISSING QUOTES, BRACKETS, OR COMMAS BEFORE ASKING FOR HELP!!");
+            U.error(CC.YELLOW + CC.LINE);
+            U.error(CC.RED + e.toString());
+            return false;
+        }
+        return true;
+    }
+
+    public static CM get() {
+        return instance;
     }
 
 
